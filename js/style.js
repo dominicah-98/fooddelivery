38449dff8c90164b9be6a63f49f380eae954fa45
Dominica Halder <richagomes1998@gmail.com>
2021-07-24 11:24:56 +0530

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,28 @@
+$(window).scroll(function() {
+    var x = document.getElementById("nav");
+    if(this.scrollY <= 10){
+        x.className = "";
+    }
+    else{
+        x.className = "scrolled";
+    }
+});
+
+$('#hamburger-menu').click(() => {
+  $('#hamburger-menu').toggleClass('responsive');
+  $('#myTopnav').toggleClass('responsive');
+})
+
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
