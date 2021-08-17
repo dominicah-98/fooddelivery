1ca0124f05dd9739e487c4cf2d57b6d68a2cbdcd
Dominica Halder <richagomes1998@gmail.com>
2021-08-17 13:40:32 +0530

    Update style.js

--- js/style.js
+++ js/style.js
@@ -13,16 +13,32 @@ $('#hamburger-menu').click(() => {
   $('#myTopnav').toggleClass('responsive');
 })
 
-var mybutton = document.getElementById("myBtn");
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
+// var mybutton = document.getElementById("myBtn");
+// window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
+
+$(document).ready(function(){
+
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 40) {
+      $('#myBtn').fadeIn();
+
+  }else{
+    $('#myBtn').fadeOut();
   }
-}
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+  });
+
+  $('#myBtn').click(function(){
+    $('html,body').animate({scrollTop : 0},100);
+  });
+});
